8f2e3f8012041af0ac17a81ecc9ff39c731a25ec
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2019-06-25 15:21:46 +0200

    adding words

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(5)
+
+let words = ['tissu', 'par', 'pars', 'faire', 'la', 'pose', 'terre', 'le', 'les', 'mes', 'arbre', 'finir', 'huile', 'quel', 'avoir', 'palme', 'seul', 'tilleul', 'comprendre',
+'utile', 'perdre', 'falloir', 'pratique', 'nu', 'qualité', 'outre', 'enterrement', 'damin', 'glage', 'chaleur', 'ici', 'tous', 'metro', 'vent', 'itinéraire', 'parallèle', 'sanguin',
+'sérum', 'vernis', 'poudre', 'plante', 'radiateur', 'prise', 'ville', 'course', ]
